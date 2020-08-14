744baa7e993a1620e762d306c7eaccceec783b75
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-13 19:34:55 -0500

    Created query to gather all customers and thier addresses

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,5 +1,7 @@
 --1. Get all customers and their addresses.
-
+SELECT *
+FROM "customers"
+    JOIN "addresses" ON "customers"."id" = "addresses"."customer_id";
 
 
 --2. Get all orders and their line items (orders, quantity and product).
