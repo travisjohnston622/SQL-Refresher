53459f0d3ddad97635e4e091b000b7ed5e248135
Travis Johnston <travisjohnston4588@icloud.com>
2020-08-13 19:54:35 -0500

    Created query to gather all warehouses that have product_id 6(diet pepsi)

--- joins-solution.sql
+++ joins-solution.sql
@@ -18,7 +18,10 @@ WHERE "warehouse_product"."product_id" = 5;
 
 
 --4. Which warehouses have diet pepsi?
-
+SELECT *
+FROM "warehouse"
+    JOIN "warehouse_product" ON "warehouse"."id" = "warehouse_product"."warehouse_id"
+WHERE "warehouse_product"."product_id" = 6;
 
 
 --5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
